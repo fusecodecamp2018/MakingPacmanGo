c3d9e757e01b84b67bad4868a2c6577f6d31794a
Nathan Zender <nathan.zender@cardinalhealth.com>
2018-03-15 22:30:32 -0400

    Implement the starts of add ghost

--- pacman-game-server/server.go
+++ pacman-game-server/server.go
@@ -19,11 +19,18 @@ func trackPacman(w http.ResponseWriter, r *http.Request){
 	w.WriteHeader(http.StatusOK)
 }
 
+func addGhost(w http.ResponseWriter, r *http.Request){
+	fmt.Println(">> ADD GHOST << ")
+	fmt.Println(r.URL.Query())
+
+	w.WriteHeader(http.StatusOK)
+}
+
 func trackGhost(w http.ResponseWriter, r *http.Request){
 	fmt.Println(">> TRACK GHOST << ")
 	fmt.Println(r.URL.Query())
 
-	w.WriteHeader(http.StatusFound)
+	w.WriteHeader(http.StatusOK)
 }
 
 func moveGhost(w http.ResponseWriter, r *http.Request){
@@ -42,6 +49,7 @@ func isGameOver(w http.ResponseWriter, r *http.Request){
 
 func main() {
 	http.HandleFunc("/api/startGame", startGame)
+	http.HandleFunc("/api/addGhost", addGhost)
 	http.HandleFunc("/api/trackGhost", trackGhost)
 	http.HandleFunc("/api/moveGhost", moveGhost)
 	http.HandleFunc("/api/trackPacman", trackPacman)
