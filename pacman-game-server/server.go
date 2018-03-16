ecfaa20e3e432b2114a7382ce753e712d59368e2
Nathan Zender <nathan.zender@cardinalhealth.com>
2018-03-15 20:57:51 -0400

    Stubbed api implementations for now just to make sure it does what i want them to do....will build bare minimum to get the game to work.

--- pacman-game-server/server.go
+++ pacman-game-server/server.go
@@ -2,9 +2,50 @@ package main
 
 import (
 	"net/http"
+	"fmt"
 )
 
+func startGame(w http.ResponseWriter, r *http.Request){
+	fmt.Println(">> START GAME << ")
+	fmt.Println(r.URL.Query())
+
+	w.WriteHeader(http.StatusNotAcceptable)
+}
+
+func trackGhost(w http.ResponseWriter, r *http.Request){
+	fmt.Println(">> TRACK GHOST << ")
+	fmt.Println(r.URL.Query())
+
+	w.WriteHeader(http.StatusFound)
+}
+
+func moveGhost(w http.ResponseWriter, r *http.Request){
+	fmt.Println(">> MOVE GHOST << ")
+	fmt.Println(r.URL.Query())
+
+	w.WriteHeader(http.StatusConflict)
+}
+
+func trackPacman(w http.ResponseWriter, r *http.Request){
+	fmt.Println(">> TRACK PACMAN << ")
+	fmt.Println(r.URL.Query())
+
+	w.WriteHeader(http.StatusAlreadyReported)
+}
+
+func isGameOver(w http.ResponseWriter, r *http.Request){
+	fmt.Println(">> GAME OVER << ")
+	fmt.Println(r.URL.Query())
+
+	w.WriteHeader(http.StatusAccepted)
+}
+
 func main() {
+	http.HandleFunc("/api/startGame", startGame)
+	http.HandleFunc("/api/trackGhost", trackGhost)
+	http.HandleFunc("/api/moveGhost", moveGhost)
+	http.HandleFunc("/api/trackPacman", trackPacman)
+	http.HandleFunc("/api/isGameOver", isGameOver)
 	http.Handle("/", http.FileServer(http.Dir("../pacman")))
 	http.ListenAndServe(":3000", nil)
 }
